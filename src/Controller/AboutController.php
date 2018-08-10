7aeccaadedd653df315656e8c7efaaeaa19c9287
alexis gueudre <alexisuper@hotmail.fr>
2018-08-10 14:11:36 +0200

    controller: front AboutController

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -12,15 +12,7 @@ class AboutController extends Controller
      */
 
 
-    /*
-    Ancienne fonction modifiée dessous, CPM, à supprimer:
-        public function index()
-    {
-        return $this->render('about/index.html.twig', [
-            'controller_name' => 'AboutController',
-        ]);
-    }
-    */
+   
     public function about()
     {
         return $this->render('about/about.html.twig', [
